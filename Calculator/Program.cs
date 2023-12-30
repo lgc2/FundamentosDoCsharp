131a47daec64376d7991ea3487fc18367a11f2ea
lgc2 <lgustavoccarvalho@gmail.com>
2023-12-30 11:49:58 -0300

    first commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Qual operação deseja executar?");
+            Console.WriteLine("[1] Soma");
+            Console.WriteLine("[2] Subtração");
+            Console.WriteLine("[3] Multiplicação");
+            Console.WriteLine("[4] Divisão");
+            Console.WriteLine("[5] Sair");
+
+            Console.WriteLine("--------------------------------------");
+
+            Console.WriteLine("Digite o número referente à operação desejada e tecle enter.");
+            short res = short.Parse(Console.ReadLine());
+
+            switch (res)
+            {
+                case 1: Operacao(EOperacao.Soma); break;
+                case 2: Operacao(EOperacao.Subtracao); break;
+                case 3: Operacao(EOperacao.Multiplicacao); break;
+                case 4: Operacao(EOperacao.Divisao); break;
+                case 5: System.Environment.Exit(0); break;
+                default: Menu(); break;
+            }
+        }
+
+        static void Operacao(EOperacao operacao)
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro valor:");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = 0;
+            switch ((int)operacao)
+            {
+                case 1: resultado = Soma(v1, v2); break;
+                case 2: resultado = Subtracao(v1, v2); break;
+                case 3: resultado = Multiplicacao(v1, v2); break;
+                case 4: resultado = Divisao(v1, v2); break;
+                default: Console.WriteLine("Operação não econtrada!"); break;
+            }
+
+            Console.WriteLine($"O resultado da {operacao} é: {resultado}");
+
+            Console.ReadKey();
+            Menu();
+        }
+
+        static float Soma(float v1, float v2)
+        {
+            return v1 + v2;
+        }
+
+        static float Subtracao(float v1, float v2)
+        {
+            return v1 - v2;
+        }
+
+        static float Divisao(float v1, float v2)
+        {
+            return v1 / v2;
+        }
+
+        static float Multiplicacao(float v1, float v2)
+        {
+            return v1 * v2;
+        }
+    }
+
+    enum EOperacao
+    {
+        Soma = 1,
+        Subtracao = 2,
+        Multiplicacao = 3,
+        Divisao = 4
+    }
+}
+
---
+++ MeuApp/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace MeuApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Parametros opcionais de métodos/funções têm sempre que vir por último. Para adicioná-los basta atribuir valor qualquer
+
+            // Value Types armazenam o dado diretamente na memória (Stack)
+            // --> built-in, structs, enums
+
+            // Reference Types armazenam o local da memória (Heap)
+            // --> Classes, Objects, Arrays
+            // --> ao alterar valor de uma variável, afeta todos que estão apontando para aquele local de memória
+
+            var mouse = new Product(1, "Mouse Gamer", 20.75m, EProductType.Service);
+
+            Console.WriteLine(mouse.Id);
+            Console.WriteLine(mouse.Name);
+            Console.WriteLine(mouse.PriceInDolar(5m));
+            Console.WriteLine($"Type is {mouse.Type}");
+            Console.WriteLine($"Type is {(int)mouse.Type}");
+        }
+    }
+
+    struct Product
+    {
+        public int Id;
+        public string Name;
+        public decimal Price;
+        public EProductType Type;
+
+        public Product(int id, string name, decimal price, EProductType type)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Type = type;
+        }
+
+        public decimal PriceInDolar(decimal dolar)
+        {
+            return Price * dolar;
+        }
+    }
+
+    enum EProductType
+    {
+        Product = 1,
+        Service = 2
+    }
+}
