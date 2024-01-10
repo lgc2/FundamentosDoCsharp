1164f84b49b7c2ba0d0b7670038000d8588a78f4
lgc2 <lgustavoccarvalho@gmail.com>
2024-01-10 20:00:05 -0300

    feat(Data): lição sobre formatação de datas

--- ModuloDatas/Program.cs
+++ ModuloDatas/Program.cs
@@ -1,4 +1,19 @@
-﻿// var data = new DateTime(2020, 10, 12, 13, 25, 37);
+﻿var data = DateTime.UtcNow;
+var dataBrasil = data.AddHours(-3);
+
+// var dataBrasilFormatada = String.Format("{0:yy}", dataBrasil);
+// Console.WriteLine(dataBrasilFormatada);
+
+// var dataBrasilFormatada = String.Format("{0:M}", dataBrasil);
+// Console.WriteLine(dataBrasilFormatada);
+
+var dataBrasilFormatada = String.Format("{0:dd/MM/yy HH:mm}", dataBrasil);
+Console.WriteLine(dataBrasilFormatada);
+
+
+
+/*
+// var data = new DateTime(2020, 10, 12, 13, 25, 37);
 var dataUtc = DateTime.UtcNow;
 var dataLocal = dataUtc.AddHours(-24);
 
@@ -6,3 +21,4 @@ Console.WriteLine($"essa é a data UTC: {dataUtc}");
 Console.WriteLine(dataUtc.Day);
 Console.WriteLine($"essa é a data local: {dataLocal}");
 Console.WriteLine(dataLocal.Day);
+*/
