b54a307df02ac8106ab49bb560814737395dfa4f
lgc2 <lgustavoccarvalho@gmail.com>
2024-01-10 19:42:59 -0300

    feat(Datas): duas primeiras aulas

---
+++ ModuloDatas/Program.cs
@@ -0,0 +1,8 @@
+﻿// var data = new DateTime(2020, 10, 12, 13, 25, 37);
+var dataUtc = DateTime.UtcNow;
+var dataLocal = dataUtc.AddHours(-24);
+
+Console.WriteLine($"essa é a data UTC: {dataUtc}");
+Console.WriteLine(dataUtc.Day);
+Console.WriteLine($"essa é a data local: {dataLocal}");
+Console.WriteLine(dataLocal.Day);
