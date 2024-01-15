bf382fd21cc72c6d209d48ac72437dd33bb85f16
lgc2 <lgustavoccarvalho@gmail.com>
2024-01-15 20:00:30 -0300

    feat(ModuloArrays): adiciona primeira aula

---
+++ ModuloArrays/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+var meuArray = new Teste[2];
+meuArray[0] = new Teste();
+
+Console.WriteLine(meuArray[0].Id);
+Console.WriteLine(meuArray[1].Id);
+
+struct Teste
+{
+    public int Id { get; set; }
+}
+*/
+
+/*
+// var meuArray = new int[5]; // posso inicializar sem atribuir valores
+// meuArray[0] = 12;
+
+var meuArray = new int[5] { 1, 2, 3, 4, 5 };
+
+foreach (var item in meuArray)
+{
+    Console.WriteLine(item);
+}
+*/
