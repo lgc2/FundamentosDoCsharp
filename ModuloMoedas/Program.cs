aa9696bed3885f203ce219a961e2475fb803f631
lgc2 <lgustavoccarvalho@gmail.com>
2024-01-15 19:44:22 -0300

    feat(ModuloMoedas): adiciona aulas

---
+++ ModuloMoedas/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+var valor = 10536.85m;
+Console.WriteLine(Math.Round(valor));
+Console.WriteLine(Math.Ceiling(valor));
+Console.WriteLine(Math.Floor(valor));
+
+
+/*
+var valor = 10536.5m;
+Console.WriteLine(valor);
+Console.WriteLine(valor.ToString("C", CultureInfo.CreateSpecificCulture("pt-BR")));
+Console.WriteLine(valor.ToString("N", CultureInfo.CreateSpecificCulture("pt-BR")));
+*/
