81657414332a38bfd6e8b6da97238282a09a6d73
lgc2 <lgustavoccarvalho@gmail.com>
2024-01-17 21:03:31 -0300

    feat(ModuloArrays): finaliza modulo

--- ModuloArrays/Program.cs
+++ ModuloArrays/Program.cs
@@ -1,4 +1,37 @@
-﻿/*
+﻿// Value Types --> Array é Tipo de Valor
+var primeiro = new int[4];
+var segundo = new int[4];
+
+segundo[0] = primeiro[0];
+
+primeiro[0] = 23;
+
+Console.WriteLine(primeiro[0]);
+Console.WriteLine(segundo[0]);
+
+
+/*
+var funcionarios = new Funcionario[2];
+funcionarios[0] = new Funcionario() { Id = 2579, Nome = "Luiz" };
+funcionarios[1] = new Funcionario() { Id = 33579, Nome = "Letícia" };
+
+Console.WriteLine(funcionarios[0]);
+Console.WriteLine(funcionarios[0].Id);
+Console.WriteLine(funcionarios[0].Nome);
+
+foreach (var funcionario in funcionarios)
+{
+    Console.WriteLine($"Nome: {funcionario.Nome} | Id: {funcionario.Id}");
+}
+
+public struct Funcionario
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+}
+*/
+
+/*
 var meuArray = new Teste[2];
 meuArray[0] = new Teste();
 
